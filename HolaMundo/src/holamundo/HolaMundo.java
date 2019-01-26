c159dff8c584c10ce238d986b52e4bba3ff7ac14
Aaron Agullo <Aaronagullo24@gmail.com>
2019-01-26 11:20:36 +0100

    Insertamos un System.out.println

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -16,6 +16,9 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Adios");
+        System.out.println("Verde");
+        System.out.println("Rojo");
     }
     
 }
